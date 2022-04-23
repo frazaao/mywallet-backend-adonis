25049ad6f46d8d8a538af0a409e1bf6f691e98cb
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2022-04-23 18:36:37 -0300

    postgres support added

--- env.ts
+++ env.ts
@@ -26,4 +26,9 @@ export default Env.rules({
   MYSQL_USER: Env.schema.string(),
   MYSQL_PASSWORD: Env.schema.string.optional(),
   MYSQL_DB_NAME: Env.schema.string(),
+  PG_HOST: Env.schema.string({ format: 'host' }),
+  PG_PORT: Env.schema.number(),
+  PG_USER: Env.schema.string(),
+  PG_PASSWORD: Env.schema.string.optional(),
+  PG_DB_NAME: Env.schema.string(),
 })
